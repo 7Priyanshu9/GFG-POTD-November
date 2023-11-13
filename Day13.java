3d64ce234498338076f96fe8b97dab96d95bb3c1
Priyanshu <Priyanshupaul32@gmail.com>
2023-11-13 20:01:41 +0530

    Day 13 added

---
+++ Day13.java
@@ -0,0 +1,27 @@
+public class Day13{
+    public static int shortestCommonSupersequence(String X,String Y,int m,int n)
+    {
+        //Your code here
+         int[][] dp = new int[m + 1][n + 1];
+
+        // Fill the table using dynamic programming
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // The length of the shortest common supersequence is given by:
+        // m + n - length of LCS (Longest Common Subsequence)
+        int lcsLength = dp[m][n];
+        int shortestSupersequenceLength = m + n - lcsLength;
+
+        return shortestSupersequenceLength;
+    }
+}
