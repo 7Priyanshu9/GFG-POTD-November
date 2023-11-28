fddaf999c18f9a91492f70f2a4901a4ae7b0e07d
Priyanshu <Priyanshupaul32@gmail.com>
2023-11-28 09:45:33 +0530

    Day 28 added java solution

---
+++ Day28.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+class Day28{
+    int sumOfDependencies(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        
+        int count = 0;
+        for(int i = 0 ; i< adj.size(); i++){
+            count+=adj.get(i).size();
+        }
+        return count;
+    }
+}
